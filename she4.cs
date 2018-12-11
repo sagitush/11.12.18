5c8de8cd2509baa58e6c2edaea4844fe010fa9cb
sagitush <44786027+sagitush@users.noreply.github.com>
2018-12-11 18:43:32 +0200

    Add files via upload

---
+++ she4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheela_4
+{
+    class Program
+    {
+        private static int numbers(params int[] arrayofnumbers)
+        {
+            int sum = arrayofnumbers[0];
+            for (int i = 1; i < arrayofnumbers.Length; i++)
+            {
+                int azeret = 1;
+                for (int j = 0; j <=i; j++)
+                {
+                    azeret = azeret * arrayofnumbers[i];
+
+                }
+                sum = sum + azeret;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(numbers(5,4,3,8));
+        }
+    }
+}
