f9a2a307efe9b595a869078b315110a6d58cd8d5
sagitush <44786027+sagitush@users.noreply.github.com>
2018-12-11 18:39:10 +0200

    Add files via upload

---
+++ she5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheela5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            cefel(a);
+            int cefel1 = cefel(a);
+            Console.WriteLine(cefel1);
+            Console.WriteLine( cefel(a: 1, c: 10));
+        }
+
+        private static int cefel(int a, int b=1, int c=-1)
+        {
+            int d = a * b * c;
+            return d;
+        }
+    }
+}
