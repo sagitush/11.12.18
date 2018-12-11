236f4c64126794d2380928dadc076f4f5c670057
sagitush <44786027+sagitush@users.noreply.github.com>
2018-12-11 18:37:54 +0200

    Add files via upload

---
+++ she1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._12._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 5;
+            if (Booleani(a, b)==true)
+            {
+                Console.WriteLine("no module");
+            }
+            else
+            Console.WriteLine("some module");
+
+
+        }
+
+        private static bool Booleani(int a, int b)
+        {
+            bool mitchalek=true;
+            if (a%b==0)
+            {
+               return mitchalek;
+            }
+            
+            return !mitchalek;
+
+        }
+    }
+}
