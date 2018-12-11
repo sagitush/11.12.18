ba7175b716fd1d173fcc5d69075fa6d5f2f824b9
sagitush <44786027+sagitush@users.noreply.github.com>
2018-12-11 18:44:15 +0200

    Add files via upload

---
+++ she2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheela_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =3;
+            int b=2;
+            Peula(ref a,ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        private static void Peula(ref int a,ref int b)
+        {
+            a = a + 1;
+            b = b * 2;
+        }
+    }
+}
