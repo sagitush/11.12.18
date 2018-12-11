2215ca1a973d1637ddeef0669056aa3bf6da91be
sagitush <44786027+sagitush@users.noreply.github.com>
2018-12-11 18:42:45 +0200

    Add files via upload

---
+++ she3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheela3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Klita(out a,out b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        private static void Klita(out int a,out int b)
+        {
+            a = Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
